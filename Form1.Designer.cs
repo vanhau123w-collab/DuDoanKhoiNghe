56d5bb9aad73db5dfa6bea3fa361dc6ab6695de7
vanha <vanha@HARU>
2025-08-22 00:06:13 +0700

    dự đoán dựa trên môn học

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,14 +29,19 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.btnA = new System.Windows.Forms.Button();
-            this.btnB = new System.Windows.Forms.Button();
-            this.btnC = new System.Windows.Forms.Button();
-            this.btnH = new System.Windows.Forms.Button();
-            this.btnD = new System.Windows.Forms.Button();
-            this.btnR = new System.Windows.Forms.Button();
-            this.btnV = new System.Windows.Forms.Button();
-            this.btnKhac = new System.Windows.Forms.Button();
+            this.cbToan = new System.Windows.Forms.CheckBox();
+            this.cbLy = new System.Windows.Forms.CheckBox();
+            this.cbSu = new System.Windows.Forms.CheckBox();
+            this.cbVan = new System.Windows.Forms.CheckBox();
+            this.cbSinh = new System.Windows.Forms.CheckBox();
+            this.cbHoa = new System.Windows.Forms.CheckBox();
+            this.cbDia = new System.Windows.Forms.CheckBox();
+            this.cbGDCD = new System.Windows.Forms.CheckBox();
+            this.cbVe1 = new System.Windows.Forms.CheckBox();
+            this.cbVe2 = new System.Windows.Forms.CheckBox();
+            this.cbNangKhieu = new System.Windows.Forms.CheckBox();
+            this.btnDuDoan = new System.Windows.Forms.Button();
+            this.cbAnh = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // label1
@@ -44,137 +49,189 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label1.Location = new System.Drawing.Point(27, 41);
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.MaximumSize = new System.Drawing.Size(600, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(402, 45);
+            this.label1.Size = new System.Drawing.Size(596, 90);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Chọn khối ngành của bạn";
+            this.label1.Text = "Chọn các môn mà bạn thi, mình sẽ dự đoán khối nghề của bạn nhé";
             // 
-            // btnA
+            // cbToan
             // 
-            this.btnA.AutoSize = true;
-            this.btnA.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.btnA.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnA.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnA.Location = new System.Drawing.Point(47, 106);
-            this.btnA.Name = "btnA";
-            this.btnA.Size = new System.Drawing.Size(85, 34);
-            this.btnA.TabIndex = 1;
-            this.btnA.Text = "Khối A";
-            this.btnA.UseVisualStyleBackColor = true;
-            this.btnA.Click += new System.EventHandler(this.btnA_Click);
+            this.cbToan.AutoSize = true;
+            this.cbToan.Font = new System.Drawing.Font("MS UI Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbToan.ForeColor = System.Drawing.Color.IndianRed;
+            this.cbToan.Location = new System.Drawing.Point(20, 122);
+            this.cbToan.Name = "cbToan";
+            this.cbToan.Size = new System.Drawing.Size(91, 29);
+            this.cbToan.TabIndex = 11;
+            this.cbToan.Text = "Toán";
+            this.cbToan.UseVisualStyleBackColor = true;
             // 
-            // btnB
+            // cbLy
             // 
-            this.btnB.AutoSize = true;
-            this.btnB.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.btnB.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnB.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnB.Location = new System.Drawing.Point(138, 106);
-            this.btnB.Name = "btnB";
-            this.btnB.Size = new System.Drawing.Size(85, 34);
-            this.btnB.TabIndex = 2;
-            this.btnB.Text = "Khối B";
-            this.btnB.UseVisualStyleBackColor = true;
-            this.btnB.Click += new System.EventHandler(this.btnB_Click);
+            this.cbLy.AutoSize = true;
+            this.cbLy.Font = new System.Drawing.Font("MS UI Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbLy.ForeColor = System.Drawing.Color.IndianRed;
+            this.cbLy.Location = new System.Drawing.Point(20, 157);
+            this.cbLy.Name = "cbLy";
+            this.cbLy.Size = new System.Drawing.Size(61, 29);
+            this.cbLy.TabIndex = 16;
+            this.cbLy.Text = "Lý";
+            this.cbLy.UseVisualStyleBackColor = true;
             // 
-            // btnC
+            // cbSu
             // 
-            this.btnC.AutoSize = true;
-            this.btnC.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.btnC.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnC.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnC.Location = new System.Drawing.Point(229, 106);
-            this.btnC.Name = "btnC";
-            this.btnC.Size = new System.Drawing.Size(84, 34);
-            this.btnC.TabIndex = 3;
-            this.btnC.Text = "Khối C";
-            this.btnC.UseVisualStyleBackColor = true;
-            this.btnC.Click += new System.EventHandler(this.btnC_Click);
+            this.cbSu.AutoSize = true;
+            this.cbSu.Font = new System.Drawing.Font("MS UI Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbSu.ForeColor = System.Drawing.Color.IndianRed;
+            this.cbSu.Location = new System.Drawing.Point(147, 157);
+            this.cbSu.Name = "cbSu";
+            this.cbSu.Size = new System.Drawing.Size(62, 29);
+            this.cbSu.TabIndex = 17;
+            this.cbSu.Text = "Sử";
+            this.cbSu.UseVisualStyleBackColor = true;
             // 
-            // btnH
+            // cbVan
             // 
-            this.btnH.AutoSize = true;
-            this.btnH.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.btnH.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnH.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnH.Location = new System.Drawing.Point(139, 146);
-            this.btnH.Name = "btnH";
-            this.btnH.Size = new System.Drawing.Size(86, 34);
-            this.btnH.TabIndex = 4;
-            this.btnH.Text = "Khối H";
-            this.btnH.UseVisualStyleBackColor = true;
-            this.btnH.Click += new System.EventHandler(this.btnH_Click);
+            this.cbVan.AutoSize = true;
+            this.cbVan.Font = new System.Drawing.Font("MS UI Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbVan.ForeColor = System.Drawing.Color.IndianRed;
+            this.cbVan.Location = new System.Drawing.Point(147, 122);
+            this.cbVan.Name = "cbVan";
+            this.cbVan.Size = new System.Drawing.Size(78, 29);
+            this.cbVan.TabIndex = 18;
+            this.cbVan.Text = "Văn";
+            this.cbVan.UseVisualStyleBackColor = true;
             // 
-            // btnD
+            // cbSinh
             // 
-            this.btnD.AutoSize = true;
-            this.btnD.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.btnD.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnD.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnD.Location = new System.Drawing.Point(320, 106);
-            this.btnD.Name = "btnD";
-            this.btnD.Size = new System.Drawing.Size(86, 34);
-            this.btnD.TabIndex = 4;
-            this.btnD.Text = "Khối D";
-            this.btnD.UseVisualStyleBackColor = true;
-            this.btnD.Click += new System.EventHandler(this.btnD_Click);
+            this.cbSinh.AutoSize = true;
+            this.cbSinh.Font = new System.Drawing.Font("MS UI Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbSinh.ForeColor = System.Drawing.Color.IndianRed;
+            this.cbSinh.Location = new System.Drawing.Point(20, 227);
+            this.cbSinh.Name = "cbSinh";
+            this.cbSinh.Size = new System.Drawing.Size(84, 29);
+            this.cbSinh.TabIndex = 19;
+            this.cbSinh.Text = "Sinh";
+            this.cbSinh.UseVisualStyleBackColor = true;
             // 
-            // btnR
+            // cbHoa
             // 
-            this.btnR.AutoSize = true;
-            this.btnR.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.btnR.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnR.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnR.Location = new System.Drawing.Point(47, 146);
-            this.btnR.Name = "btnR";
-            this.btnR.Size = new System.Drawing.Size(86, 34);
-            this.btnR.TabIndex = 5;
-            this.btnR.Text = "Khối R";
-            this.btnR.UseVisualStyleBackColor = true;
-            this.btnR.Click += new System.EventHandler(this.btnR_Click);
+            this.cbHoa.AutoSize = true;
+            this.cbHoa.Font = new System.Drawing.Font("MS UI Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbHoa.ForeColor = System.Drawing.Color.IndianRed;
+            this.cbHoa.Location = new System.Drawing.Point(20, 192);
+            this.cbHoa.Name = "cbHoa";
+            this.cbHoa.Size = new System.Drawing.Size(78, 29);
+            this.cbHoa.TabIndex = 20;
+            this.cbHoa.Text = "Hóa";
+            this.cbHoa.UseVisualStyleBackColor = true;
             // 
-            // btnV
+            // cbDia
             // 
-            this.btnV.AutoSize = true;
-            this.btnV.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.btnV.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnV.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnV.Location = new System.Drawing.Point(231, 146);
-            this.btnV.Name = "btnV";
-            this.btnV.Size = new System.Drawing.Size(85, 34);
-            this.btnV.TabIndex = 5;
-            this.btnV.Text = "Khối V";
-            this.btnV.UseVisualStyleBackColor = true;
-            this.btnV.Click += new System.EventHandler(this.btnV_Click);
+            this.cbDia.AutoSize = true;
+            this.cbDia.Font = new System.Drawing.Font("MS UI Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbDia.ForeColor = System.Drawing.Color.IndianRed;
+            this.cbDia.Location = new System.Drawing.Point(147, 192);
+            this.cbDia.Name = "cbDia";
+            this.cbDia.Size = new System.Drawing.Size(69, 29);
+            this.cbDia.TabIndex = 21;
+            this.cbDia.Text = "Địa";
+            this.cbDia.UseVisualStyleBackColor = true;
             // 
-            // btnKhac
+            // cbGDCD
             // 
-            this.btnKhac.AutoSize = true;
-            this.btnKhac.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.btnKhac.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnKhac.ForeColor = System.Drawing.SystemColors.MenuHighlight;
-            this.btnKhac.Location = new System.Drawing.Point(336, 146);
-            this.btnKhac.Name = "btnKhac";
-            this.btnKhac.Size = new System.Drawing.Size(70, 34);
-            this.btnKhac.TabIndex = 10;
-            this.btnKhac.Text = "Khác";
-            this.btnKhac.UseVisualStyleBackColor = true;
-            this.btnKhac.Click += new System.EventHandler(this.btnKhac_Click);
+            this.cbGDCD.AutoSize = true;
+            this.cbGDCD.Font = new System.Drawing.Font("MS UI Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbGDCD.ForeColor = System.Drawing.Color.IndianRed;
+            this.cbGDCD.Location = new System.Drawing.Point(147, 227);
+            this.cbGDCD.Name = "cbGDCD";
+            this.cbGDCD.Size = new System.Drawing.Size(104, 29);
+            this.cbGDCD.TabIndex = 22;
+            this.cbGDCD.Text = "GDCD";
+            this.cbGDCD.UseVisualStyleBackColor = true;
+            // 
+            // cbVe1
+            // 
+            this.cbVe1.AutoSize = true;
+            this.cbVe1.Font = new System.Drawing.Font("MS UI Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbVe1.ForeColor = System.Drawing.Color.IndianRed;
+            this.cbVe1.Location = new System.Drawing.Point(276, 122);
+            this.cbVe1.Name = "cbVe1";
+            this.cbVe1.Size = new System.Drawing.Size(86, 29);
+            this.cbVe1.TabIndex = 23;
+            this.cbVe1.Text = "Vẽ 1";
+            this.cbVe1.UseVisualStyleBackColor = true;
+            // 
+            // cbVe2
+            // 
+            this.cbVe2.AutoSize = true;
+            this.cbVe2.Font = new System.Drawing.Font("MS UI Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbVe2.ForeColor = System.Drawing.Color.IndianRed;
+            this.cbVe2.Location = new System.Drawing.Point(276, 157);
+            this.cbVe2.Name = "cbVe2";
+            this.cbVe2.Size = new System.Drawing.Size(86, 29);
+            this.cbVe2.TabIndex = 24;
+            this.cbVe2.Text = "Vẽ 2";
+            this.cbVe2.UseVisualStyleBackColor = true;
+            // 
+            // cbNangKhieu
+            // 
+            this.cbNangKhieu.AutoSize = true;
+            this.cbNangKhieu.Font = new System.Drawing.Font("MS UI Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbNangKhieu.ForeColor = System.Drawing.Color.IndianRed;
+            this.cbNangKhieu.Location = new System.Drawing.Point(276, 192);
+            this.cbNangKhieu.Name = "cbNangKhieu";
+            this.cbNangKhieu.Size = new System.Drawing.Size(290, 29);
+            this.cbNangKhieu.TabIndex = 25;
+            this.cbNangKhieu.Text = "Năng khiếu nghệ thuật";
+            this.cbNangKhieu.UseVisualStyleBackColor = true;
+            // 
+            // btnDuDoan
+            // 
+            this.btnDuDoan.AutoSize = true;
+            this.btnDuDoan.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnDuDoan.Font = new System.Drawing.Font("Microsoft YaHei", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDuDoan.ForeColor = System.Drawing.Color.Gold;
+            this.btnDuDoan.Location = new System.Drawing.Point(466, 292);
+            this.btnDuDoan.Name = "btnDuDoan";
+            this.btnDuDoan.Size = new System.Drawing.Size(132, 43);
+            this.btnDuDoan.TabIndex = 27;
+            this.btnDuDoan.Text = "Dự đoán";
+            this.btnDuDoan.UseVisualStyleBackColor = true;
+            this.btnDuDoan.Click += new System.EventHandler(this.btnDuDoan_Click);
+            // 
+            // cbAnh
+            // 
+            this.cbAnh.AutoSize = true;
+            this.cbAnh.Font = new System.Drawing.Font("MS UI Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbAnh.ForeColor = System.Drawing.Color.IndianRed;
+            this.cbAnh.Location = new System.Drawing.Point(20, 262);
+            this.cbAnh.Name = "cbAnh";
+            this.cbAnh.Size = new System.Drawing.Size(79, 29);
+            this.cbAnh.TabIndex = 28;
+            this.cbAnh.Text = "Anh";
+            this.cbAnh.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(480, 238);
-            this.Controls.Add(this.btnKhac);
-            this.Controls.Add(this.btnV);
-            this.Controls.Add(this.btnR);
-            this.Controls.Add(this.btnD);
-            this.Controls.Add(this.btnH);
-            this.Controls.Add(this.btnC);
-            this.Controls.Add(this.btnB);
-            this.Controls.Add(this.btnA);
+            this.ClientSize = new System.Drawing.Size(610, 347);
+            this.Controls.Add(this.cbAnh);
+            this.Controls.Add(this.btnDuDoan);
+            this.Controls.Add(this.cbNangKhieu);
+            this.Controls.Add(this.cbVe2);
+            this.Controls.Add(this.cbVe1);
+            this.Controls.Add(this.cbGDCD);
+            this.Controls.Add(this.cbDia);
+            this.Controls.Add(this.cbHoa);
+            this.Controls.Add(this.cbSinh);
+            this.Controls.Add(this.cbVan);
+            this.Controls.Add(this.cbSu);
+            this.Controls.Add(this.cbLy);
+            this.Controls.Add(this.cbToan);
             this.Controls.Add(this.label1);
             this.ForeColor = System.Drawing.SystemColors.ActiveBorder;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
@@ -189,14 +246,19 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button btnA;
-        private System.Windows.Forms.Button btnB;
-        private System.Windows.Forms.Button btnC;
-        private System.Windows.Forms.Button btnH;
-        private System.Windows.Forms.Button btnD;
-        private System.Windows.Forms.Button btnR;
-        private System.Windows.Forms.Button btnV;
-        private System.Windows.Forms.Button btnKhac;
+        private System.Windows.Forms.CheckBox cbToan;
+        private System.Windows.Forms.CheckBox cbLy;
+        private System.Windows.Forms.CheckBox cbSu;
+        private System.Windows.Forms.CheckBox cbVan;
+        private System.Windows.Forms.CheckBox cbSinh;
+        private System.Windows.Forms.CheckBox cbHoa;
+        private System.Windows.Forms.CheckBox cbDia;
+        private System.Windows.Forms.CheckBox cbGDCD;
+        private System.Windows.Forms.CheckBox cbVe1;
+        private System.Windows.Forms.CheckBox cbVe2;
+        private System.Windows.Forms.CheckBox cbNangKhieu;
+        private System.Windows.Forms.Button btnDuDoan;
+        private System.Windows.Forms.CheckBox cbAnh;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -10,75 +10,129 @@ using System.Windows.Forms;
 
 namespace DuDoanKhoiNghe
 {
-    public partial class Form1 : Form
+public partial class Form1 : Form
+{
+    public Form1()
     {
-        public Form1()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+    }
 
-        private void btnA_Click(object sender, EventArgs e)
+    private void btnDuDoan_Click(object sender, EventArgs e)
+    {
+        List<string> MonChon = new List<string>();
+        if (cbToan.Checked) MonChon.Add("Toán");
+        if (cbLy.Checked) MonChon.Add("Lý");
+        if (cbHoa.Checked) MonChon.Add("Hóa");
+        if (cbSinh.Checked) MonChon.Add("Sinh");
+        if (cbVan.Checked) MonChon.Add("Văn");
+        if (cbSu.Checked) MonChon.Add("Sử");
+        if (cbDia.Checked) MonChon.Add("Địa");
+        if (cbGDCD.Checked) MonChon.Add("GDCD");
+        if (cbVe1.Checked) MonChon.Add("Vẽ 1");
+        if (cbVe2.Checked) MonChon.Add("Vẽ 2");
+        if (cbNangKhieu.Checked) MonChon.Add("Năng khiếu");
+        if (cbAnh.Checked) MonChon.Add("Anh");
+
+            List<List<string>> khoiA = new List<List<string>>
+{
+    new List<string> { "Toán", "Lý", "Hóa" },
+    new List<string> { "Toán", "Lý", "Anh" },
+    new List<string> { "Toán", "Hóa", "Anh" }
+};
+        List<List<string>> khoiB = new List<List<string>>
+{
+    new List<string> { "Toán", "Hóa", "Sinh" },
+    new List<string> { "Toán", "Sinh", "Lí" },
+};
+        List<List<string>> khoiC = new List<List<string>>
+{
+    new List<string> { "Văn", "Sử", "Địa" },
+    new List<string> { "Văn", "Sử", "Anh" },
+    new List<string> { "Văn", "Địa", "Anh" }
+};
+        List<List<string>> khoiD = new List<List<string>>
+{
+    new List<string> { "Toán", "Văn", "Anh"},
+    new List<string> { "Toán","Hóa", "Anh" },
+    new List<string> { "Toán","Sinh", "Anh" },
+};
+        List<List<string>> khoiH = new List<List<string>>
+{
+        new List<string> { "Toán", "Văn", "Vẽ 1" }
+};
+        List<List<string>> khoiR = new List<List<string>>
+{
+    new List<string> { "Toán", "Văn", "Vẽ 2" }
+};
+        List<List<string>> khoiV = new List<List<string>>
+{
+    new List<string> { "Toán", "Văn", "Năng khiếu" }
+};
+        bool trungKhopA = khoiA.Any(tohop => tohop.Count == MonChon.Count && !tohop.Except(MonChon).Any());
+        bool trungKhopB = khoiB.Any(tohop => tohop.Count == MonChon.Count && !tohop.Except(MonChon).Any());
+        bool trungKhopC = khoiC.Any(tohop => tohop.Count == MonChon.Count && !tohop.Except(MonChon).Any());
+        bool trungKhopD = khoiD.Any(tohop => tohop.Count == MonChon.Count && !tohop.Except(MonChon).Any());
+        bool trungKhopH = khoiH.Any(tohop => tohop.Count == MonChon.Count && !tohop.Except(MonChon).Any());
+        bool trungKhopR = khoiR.Any(tohop => tohop.Count == MonChon.Count && !tohop.Except(MonChon).Any());
+        bool trungKhopV = khoiV.Any(tohop => tohop.Count == MonChon.Count && !tohop.Except(MonChon).Any());
+
+        if (trungKhopA)
         {
             this.Hide();
             FormKhoiA frm = new FormKhoiA();
             frm.ShowDialog();
             this.Show();
-        }
-
-        private void btnB_Click(object sender, EventArgs e)
+            }
+        else if (trungKhopB)
         {
             this.Hide();
             FormKhoiB frm = new FormKhoiB();
             frm.ShowDialog();
             this.Show();
-        }
-
-        private void btnC_Click(object sender, EventArgs e)
+            }
+        else if (trungKhopC)
         {
             this.Hide();
             FormKhoiC frm = new FormKhoiC();
             frm.ShowDialog();
             this.Show();
-        }
-
-        private void btnD_Click(object sender, EventArgs e)
+            }
+        else if (trungKhopD)
         {
             this.Hide();
             FormKhoiD frm = new FormKhoiD();
             frm.ShowDialog();
-            this.Show();
-        }
-
-        private void btnH_Click(object sender, EventArgs e)
+                this.Show();
+            }
+        else if (trungKhopH)
         {
             this.Hide();
             FormKhoiH frm = new FormKhoiH();
             frm.ShowDialog();
-            this.Show();
-        }
-
-        private void btnV_Click(object sender, EventArgs e)
-        {
-            this.Hide();
-            FormKhoiV frm = new FormKhoiV();
-            frm.ShowDialog();
-            this.Show();
-        }
-
-        private void btnR_Click(object sender, EventArgs e)
+                this.Show();
+            }
+        else if (trungKhopR)
         {
             this.Hide();
             FormKhoiR frm = new FormKhoiR();
             frm.ShowDialog();
-            this.Show();
-        }
-
-        private void btnKhac_Click(object sender, EventArgs e)
+                this.Show();
+            }
+        else if (trungKhopV)
+        {
+            this.Hide();
+            FormKhoiV frm = new FormKhoiV();
+            frm.ShowDialog();
+                this.Show();
+            }
+        else
         {
             this.Hide();
             FormError frm = new FormError();
             frm.ShowDialog();
-            this.Show();
-        }
+                this.Show();
+            }
+
     }
+}
 }
